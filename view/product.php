610a94d78e5083e9ffeb3c8586fcff5435944c93
Nzube Anioke <nzube.anioke@ashesi.edu.gh>
2022-11-05 13:09:16 +0000

    edit

--- view/product.php
+++ view/product.php
@@ -90,6 +90,9 @@ $Branddata = getBrands();
         <div class="d-flex justify-content-center">
             <button type="submit" name="product" class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Add Product</button>
         </div>
+    </form>
+
+    <a href="update_productdisplay.php">Edit product</a>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
