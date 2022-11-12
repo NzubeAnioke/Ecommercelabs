0d0d4c69dec9d3ff71cf39dbeec6fe56aa74f68a
Nzube Anioke <nzube.anioke@ashesi.edu.gh>
2022-11-12 22:44:19 +0000

    db cred change

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -2,15 +2,15 @@
 //Database credentials
 
 //Database credentials
-// define ('SERVERNAME', 'us-cdbr-east-06.cleardb.net');
-// define ('HOSTNAME', 'b8c3ccc85ef127');
-// define ('PASSWORD', 'aa691ef8');
-// define ('DATABASE', 'heroku_895e1461bbf91fc');
+define ('SERVERNAME', 'us-cdbr-east-06.cleardb.net');
+define ('HOSTNAME', 'b8c3ccc85ef127');
+define ('PASSWORD', 'aa691ef8');
+define ('DATABASE', 'heroku_895e1461bbf91fc');
 
 
-define ('SERVERNAME', 'localhost');
-define ('HOSTNAME', 'root');
-define ('PASSWORD', '');
-define ('DATABASE', 'shoppn');
+// define ('SERVERNAME', 'localhost');
+// define ('HOSTNAME', 'root');
+// define ('PASSWORD', '');
+// define ('DATABASE', 'shoppn');
 //mysql://b8c3ccc85ef127:aa691ef8@us-cdbr-east-06.cleardb.net/heroku_895e1461bbf91fc?reconnect=true
 ?>
